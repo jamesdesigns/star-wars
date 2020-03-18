0c733943d8838b464588deadcfe0abb420f78646
James Hooper <jameshooperdesigns@gmail.com>
2020-03-17 22:18:28 -0600

    Add blog, about, and characters page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Star Wars</h1>
+            <p>This is going to be information about this website.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/characters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Characters = () => {
+    return (
+        <div>
+            <h1>Star Wars Characters</h1>
+            <p>Favorite Characters</p>
+        </div>
+    )
+}
+
+export default Characters
