16803ed225af3bd99b72489a974d9b1cb61b4fd9
James Hooper <jameshooperdesigns@gmail.com>
2020-05-04 22:21:48 -0600

    Edit the fetch function to connect with the new Swapi api URL

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import CharacterList from "../components/characterList"
+// import CharacterList from "../components/characterList" 
 
 
 class Characters extends React.Component{
@@ -13,18 +13,18 @@ class Characters extends React.Component{
         };
     }
 
-        // componentDidMount() {
-        //     fetch('https://swapi.co/api/people/')
-        //     .then(response => {
-        //         return response.json()
-        //     })
-        //     .then(json => {
-        //         this.setState({
-        //             fetchedData: json.results,
-        //             loading: false,
-        //         })
-        //     })
-        // };
+        componentDidMount() {
+            fetch('https://swapi.py4e.com/api/people/?page=2')
+            .then(response => {
+                return response.json()
+            })
+            .then(json => {
+                this.setState({
+                    fetchedData: json.results,
+                    loading: false,
+                })
+            })
+        };
 
         render() {
             const { fetchedData } = this.state;
@@ -54,7 +54,7 @@ class Characters extends React.Component{
                     >(This list is an example of what is to come. A much larger list will have search capability<br /> and links to the characters description.)</p>
                     
 
-                    <CharacterList></CharacterList>
+                    {/* <CharacterList></CharacterList> */}
 
                     <div 
                     style={{
@@ -63,8 +63,16 @@ class Characters extends React.Component{
                         lineHeight: '1.5em'
                     }}>
                      {fetchedData.map(character => {
-                        return <p key={character.name}>{character.name} | <span style={{ color: 'gray' }}>Planet: {character.homeworld}</span></p>;
+                        return <p key={character.name}>{character.name} | <span> <a style={{ color: 'gray', textDecoration: 'none' }} href='{character.homeworld}'>{character.homeworld}</a></span></p>;
                     })} 
+                    </div>
+
+                    <div id="docBod"></div>  
+                    <div class="container text-center">
+                        <p>
+                        <button id="prev" style={{ backgroundColor: 'black', border: '1px solid white', borderRadius: '10px', color: 'white', padding: '10px', marginRight: '15px'}} className="btn btn-warning btn-sml" role="button">« Previous</button>
+                        <button id="next" style={{ backgroundColor: 'black', border: '1px solid white', borderRadius: '10px', color: 'white', padding: '10px'}} className="btn btn-warning btn-sml" role="button">Continue »</button>
+                        </p>
                     </div>
                     
                 </Layout>
